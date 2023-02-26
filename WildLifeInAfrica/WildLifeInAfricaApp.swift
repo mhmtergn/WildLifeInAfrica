5175ab1a8c1567732a0396b0924caf4ec99a6472
Mehmet Ergün <mrmehmetergun@gmail.com>
2023-02-26 16:58:24 -0500

    Initial Commit

---
+++ WildLifeInAfrica/WildLifeInAfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WildLifeInAfricaApp.swift
+//  WildLifeInAfrica
+//
+//  Created by Mehmet Ergün on 2023-02-26.
+//
+
+import SwiftUI
+
+@main
+struct WildLifeInAfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
